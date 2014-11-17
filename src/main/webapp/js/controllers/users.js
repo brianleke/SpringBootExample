2b3f5a79e8981a646dc9c4185f0d985bb9f4739d
Brain Leke <bleke@thoughtworks.com>
2014-11-17 17:27:44 +0200

    [Brain] Modifications to the html code

--- src/main/webapp/js/controllers/users.js
+++ src/main/webapp/js/controllers/users.js
@@ -1,9 +1,17 @@
 'use strict';
 
-angular.module('Users', []).controller('UserController', function($scope){
+angular.module('Users', []).controller('UserController', function($scope, UserService){
 
     $scope.initialize = function(){
-
+        UserService.getUserInformation().then(function(response){
+            $scope.user = response.data;
+        });
     };
 
+}).factory('UserService', function($http){
+    return {
+        getUserInformation: function(){
+            return $http.get('/users');
+        }
+    }
 });
